8f20e3789779e08ed7ecb5763f38a9ffb6332d7f
Zenabu Aklilu <aklilu.mekasha@gmail.com>
2021-08-27 09:02:51 -0400

    initial commit

---
+++ 101.py
@@ -0,0 +1,8 @@
+num_one = 5
+num_two = 4
+
+print(pow(num_one,num_two))
+print(num_one * num_two)
+def say_hi(first_name,last_name):
+    print('Hello!'+ first_name + " " + last_name + ".")
+say_hi('Aklilu','Mekasha')

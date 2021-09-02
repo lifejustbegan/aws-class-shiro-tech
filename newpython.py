7821daf7a2ed97ad740c7fed097727791d1fb766
Zenabu Aklilu <aklilu.mekasha@gmail.com>
2021-09-01 22:15:43 -0400

    assignment

--- newpython.py
+++ newpython.py
@@ -1,5 +1,21 @@
-def cube(num):
-    return num*num*num
+#draw write a function that check password
+#password string, not less than 8 and greater than 14
+pwd=str(input("enter a password:"))
+def my_password(pwd):
+        if len(pwd)>=8 and len(pwd)<14:
+                if type(pwd)==str:
+                        print("congradulation you have entered a correct password")
+        # elif len(pwd)<=8:
+        #                   print("passord must be above 8 character")
+        # elif len(pwd)>14:
+        #                   print("password must be less than 14 character")                 
+        # elif pwd != "ilovepython" :
+        #                  print("please enter another pwd")
+        else:
+                print("please enter a correct password")
+                print("password hint:password must be greater than 8 or <14 characters")
+        
+        
+        
 
-result = cube(8)
-print(result)
+print(my_password(pwd))
